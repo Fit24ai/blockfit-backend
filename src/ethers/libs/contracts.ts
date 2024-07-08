d5a8c03633079b7022cbced48646f2f3498016f2
luffylovesmeat <uddeshyatiwari9085@gmail.com>
2024-07-08 20:18:59 +0530

    prod vars

--- src/ethers/libs/contracts.ts
+++ src/ethers/libs/contracts.ts
@@ -1,5 +1,13 @@
-export const IcoContract = '0x53A194AC678d190cDad6d99A4f7ad193e8621A2a';
+// * DEV
+// export const IcoContract = '0x53A194AC678d190cDad6d99A4f7ad193e8621A2a';
+// export const EthereumPaymentContract =
+//   '0xA47010B505f8C5696eC6C95a7Bfe9E28C977EC6E';
+// export const BinancePaymentContract =
+//   '0x15600Fc6bd21c54b3F8cBF6a04562C2021f0d3F2';
+
+// * PROD
+export const IcoContract = '0x56D7B993D4783ae981D57190626aA38E04886671';
 export const EthereumPaymentContract =
-  '0xA47010B505f8C5696eC6C95a7Bfe9E28C977EC6E';
+  '0xe70976b7d2ea05972740b4a3e8E04ab268c23A99';
 export const BinancePaymentContract =
-  '0x15600Fc6bd21c54b3F8cBF6a04562C2021f0d3F2';
+  '0x66d5f9804cA50da3Fa06E155bedaDa5f8F36bafD';
