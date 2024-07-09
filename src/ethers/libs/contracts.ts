4e87627551e8f6aad8b0a7cbcc172e674c991d9c
luffylovesmeat <uddeshyatiwari9085@gmail.com>
2024-07-09 17:08:33 +0530

    updating payment contract

--- src/ethers/libs/contracts.ts
+++ src/ethers/libs/contracts.ts
@@ -8,6 +8,6 @@
 // * PROD
 export const IcoContract = '0x56D7B993D4783ae981D57190626aA38E04886671';
 export const EthereumPaymentContract =
-  '0xe70976b7d2ea05972740b4a3e8E04ab268c23A99';
+  '0xC8368489d0050b0F2947d5ece5CCA26e285d4C5F';
 export const BinancePaymentContract =
   '0x66d5f9804cA50da3Fa06E155bedaDa5f8F36bafD';
