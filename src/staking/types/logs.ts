aca14e1fa8e6445b7c01d6ee3edf67224721f76c
YashT1005 <yashthakur10052@gmail.com>
2024-09-19 19:13:40 +0530

    changes

--- src/staking/types/logs.ts
+++ src/staking/types/logs.ts
@@ -1,21 +1,21 @@
-import { TransactionStatusEnum } from "src/types/transaction";
+import { TransactionStatusEnum } from 'src/types/transaction';
 
-export interface IRefStakeLogs{
-    stakeId: number;
-    walletAddress: string;
-    amount: number;
-    apr: number;
-    poolType: number;
-    startTime: number;
-    stakeDuration: number;
-    txHash: string;
-    isReferred: boolean;
-    level: number;
-    refId: number;
-    transactionStatus:TransactionStatusEnum
+export interface IRefStakeLogs {
+  stakeId: number;
+  walletAddress: string;
+  amount: number;
+  apr: number;
+  poolType: number;
+  startTime: number;
+  stakeDuration: number;
+  txHash: string;
+  isReferred: boolean;
+  level: number;
+  refId: number;
+  transactionStatus: TransactionStatusEnum;
 }
 
-export interface IClaimedRewardForStake{
+export interface IClaimedRewardForStake {
   // stakeId: number;
   walletAddress: string;
   amount: number;
